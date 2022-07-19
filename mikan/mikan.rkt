8aca27290cc7650cd04b063ca36814915ea445e9
cycloidzzz <cycloidz@126.com>
2022-07-20 00:50:08 +0800

    feat(mikan/mikan.rkt): add assign homes pass for pseudo-x86 ir

--- mikan/mikan.rkt
+++ mikan/mikan.rkt
@@ -99,6 +99,8 @@
     (match e
       [(? fixnum?) `(int ,e)]
       [(? symbol?) `(var ,e)]
+      [`(return ,var)
+       `((movq (var ,var) (reg rax)))]
       [`(assign ,var ,x) #:when (symbol? x)
                          (define new-var ((select-instructions) var))
                          (cond [(eq? var x) '()]
@@ -120,7 +122,7 @@
                [else
                 `((movq ,new-var2 ,new-var)
                   (addq ,new-var1 ,new-var))]))]
-      [`(assign ,var (op, var1))
+      [`(assign ,var (,op ,var1))
        (let ([new-var ((select-instructions) var)]
              [new-var1 ((select-instructions) var1)])
          (cond [(eq? var var1)
@@ -133,6 +135,33 @@
          `(program ,ss ,@new-xs))]
       [else
        (error `select-instructions "expect valid pseudo-x86 instruction, not ~a" e)])))
+
+;;; Assign homes pass
+(define (assign-homes homes)
+  (define first-offset 8)
+  (define variable-size 8)
+  (define (make-stack-offset homes x)
+    (- (+ first-offset
+          (* variable-size
+             (hash-ref homes x)))))
+  (λ (e)
+    (match e
+      [`(int ,n) e]
+      [`(reg ,r) e]
+      [`(var ,x)
+       `(deref rbp ,(make-stack-offset homes x))]
+      [`(callq read_int) e]
+      [`(program ,es ,xs ...)
+       (define es-loc (stream->list (in-range 0 (length es))))
+       (let ([new-homes
+              (make-hash (map cons es es-loc))])
+         (define new-xs (map (assign-homes new-homes) xs))
+         `(program ,(length es) ,@new-xs))]
+      [`(,op ,(app (assign-homes homes) es) ...)
+       `(,op ,@es)]
+      [else
+       (error `assign-homes "invalid C0 instruction ~a" e)])))
+
 
 (define text-test1
   `(program
@@ -152,10 +181,16 @@
 (displayln ((interpreter-R1 '()) uniquify-test1))
 
 ;;; TODO (cycloidz): refactoring unittest with rackunit.
-(define flatten-test1 `(program
-                        (+ 52 (- 10))))
-(define flatten-test1-result ((flatten #t) flatten-test1))
-(displayln flatten-test1-result)
+(displayln "Start of Test 1 : ****************************************")
+(define test1 `(program
+                (+ 52 (- 10))))
+(define test1-uniquify-result ((uniquify '()) test1))
+(define test1-flatten-result ((flatten #t) test1-uniquify-result))
+(define test1-select-inst-result ((select-instructions) test1-flatten-result))
+(displayln test1-select-inst-result)
+(define test1-assign-homes-result ((assign-homes '()) test1-select-inst-result))
+(displayln test1-assign-homes-result)
+(displayln "End of Test 1 : *******************************************")
 
 (define flatten-test2 `(program
                         (let ([x (+ (- 10) 11)])
@@ -178,5 +213,8 @@
             (assign x (read))
             (assign y (read))
             (assign z (+ x y))))
-(define select-instructions-result1 ((select-instructions) select-instructions-test1))
-(displayln select-instructions-result1)
+(define select-inst-result1 ((select-instructions) select-instructions-test1))
+(displayln select-inst-result1)
+
+(define assign-homes-result1 ((assign-homes '()) select-inst-result1))
+(displayln assign-homes-result1)
